bdf26103629abaa96c1fee0c6b34ac8e20276ff4
menezesglesia <87841432+menezesglesia@users.noreply.github.com>
2021-09-30 21:08:12 -0300

    Criado usando o Colaboratory

---
+++ discurso Luther king.py
@@ -0,0 +1,137 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled3.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyMPcEpI+fFB18KBvF8ixKG2",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/menezesglesia/livro-receitas/blob/master/discurso%20Luther%20king.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "EgEjDUrhLFcv"
+      },
+      "source": [
+        ""
+      ],
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "85CHM7tzLH2c"
+      },
+      "source": [
+        "Estou feliz por estar hoje com vocês num evento que entrará para a história como a maior demonstração pela liberdade na história de nosso país.\n",
+        "\n",
+        "Há cem anos, um grande americano, sob cuja simbólica sombra nos encontramos, assinou a Proclamação da Emancipação. Esse decreto fundamental foi como um grande raio de luz de esperança para milhões de escravos negros que tinham sido marcados a ferro nas chamas de uma vergonhosa injustiça. Veio como uma aurora feliz para pôr fim à longa noite de cativeiro.\n",
+        "\n",
+        "Mas, cem anos mais tarde, devemos encarar a trágica realidade de que o negro ainda não é livre. Cem anos mais tarde, a vida do negro está ainda infelizmente dilacerada pelas algemas da segregação e pelas correntes da discriminação.\n",
+        "\n",
+        "Cem anos mais tarde, o negro ainda vive numa ilha isolada de pobreza no meio de um vasto oceano de prosperidade material. Cem anos mais tarde, o negro ainda definha nas margens da sociedade americana estando exilado em sua própria terra. Por isso, encontramo-nos aqui hoje para dramatizar essa terrível condição.\n",
+        "\n",
+        "De certo modo, viemos à capital do nosso país para descontar um cheque. Quando os arquitetos da nossa república escreveram as magníficas palavras da Constituição e a Declaração da Independência, eles estavam a assinar uma nota promissória da qual todo americano seria herdeiro. Essa nota foi uma promessa de que todos os homens teriam garantia aos direitos inalienáveis de \"vida, liberdade e à procura de felicidade\".\n",
+        "\n",
+        "É óbvio que a América de hoje ainda não pagou essa nota promissória no que concerne aos seus cidadãos de cor. Em vez de honrar esse compromisso sagrado, a América entregou ao povo negro um cheque inválido devolvido com a seguinte inscrição: \"Saldo insuficiente\".\n",
+        "\n",
+        "Porém recusamo-nos a acreditar que o banco da justiça abriu falência. Recusamo-nos a acreditar que não haja dinheiro suficiente nos grandes cofres de oportunidade desse país. Então viemos para descontar esse cheque, um cheque que nos dará à vista as riquezas da liberdade e a segurança da justiça.\n",
+        "\n",
+        "Viemos também para este lugar sagrado para lembrar à América da clara urgência do agora. Não é hora de se dar ao luxo de procrastinar ou de tomar o remédio tranquilizante do gradualismo. Agora é tempo de tornar reais as promessas da democracia.\n",
+        "\n",
+        "Agora é hora de sair do vale escuro e desolado da segregação para o caminho iluminado da justiça racial. Agora é hora [aplausos] de retirar a nossa nação das areias movediças da injustiça racial para a sólida rocha da fraternidade. Agora é hora de transformar a justiça em realidade para todos os filhos de Deus.\n",
+        "\n",
+        "Seria fatal para a nação não levar a sério a urgência desse momento. Esse verão sufocante da insatisfação legítima do negro não passará até que chegue o revigorante outono da liberdade e igualdade. Mil novecentos e sessenta e três não é um fim, mas um começo. E aqueles que creem que o negro só precisava desabafar e que agora ficará sossegado, acordarão sobressaltados se o país voltar ao ritmo normal.\n",
+        "\n",
+        "\n",
+        "\n",
+        "Não haverá nem descanso nem tranquilidade na América até o negro adquirir seus direitos como cidadão. Os turbilhões da revolta continuarão a sacudir os alicerces do nosso país até que o resplandecente dia da justiça desponte.\n",
+        "\n",
+        "Há algo, porém, que devo dizer a meu povo, que se encontra no caloroso limiar que conduz ao palácio da justiça: no processo de ganhar o nosso legítimo lugar não devemos ser culpados de atos errados. Não tentemos satisfazer a sede de liberdade bebendo da taça da amargura e do ódio. Devemos sempre conduzir nossa luta no nível elevado da dignidade e disciplina.\n",
+        "\n",
+        "Não devemos deixar que o nosso protesto criativo se degenere na violência física. Repetidas vezes, teremos que nos erguer às alturas majestosas para encontrar a força física com a força da alma.\n",
+        "\n",
+        "Esta nova militância maravilhosa que engolfou a comunidade negra não nos deve levar a desconfiar de todas as pessoas brancas, pois muitos dos irmãos brancos, como se vê pela presença deles aqui, hoje, estão conscientes de que seus destinos estão ligados ao nosso destino.\n",
+        "\n",
+        "E estão conscientes de que sua liberdade está intrinsicamente ligada à nossa liberdade. Não podemos caminhar sozinhos. À medida que caminhamos, devemos assumir o compromisso de marcharmos em frente. Não podemos retroceder.\n",
+        "\n",
+        "Há quem pergunte aos defensores dos direitos civis: \"Quando é que ficarão satisfeitos?\" Não estaremos satisfeitos enquanto o negro for vítima dos indescritíveis horrores da brutalidade policial. Jamais poderemos estar satisfeitos enquanto os nossos corpos, cansados com as fadigas da viagem, não conseguirem ter acesso aos hotéis de beira de estrada e das cidades.\n",
+        "\n",
+        "Não poderemos estar satisfeitos enquanto a mobilidade básica do negro for passar de um gueto pequeno para um maior. Não podemos estar satisfeitos enquanto nossas crianças forem destituídas de sua individualidade e privadas de sua dignidade por placas onde se lê \"somente para brancos\".\n",
+        "\n",
+        "Não poderemos estar satisfeitos enquanto um negro no Mississippi não puder votar e um negro em Nova Iorque achar que não há nada pelo qual valha a pena votar. Não, não, não estamos satisfeitos e só estaremos satisfeitos quando \"a justiça correr como a água e a retidão como uma poderosa corrente\".\n",
+        "\n",
+        "Eu sei muito bem que alguns de vocês chegaram aqui após muitas dificuldades e tribulações. Alguns de vocês acabaram de sair de pequenas celas de prisão. Alguns de vocês vieram de áreas onde a sua procura de liberdade lhes deixou marcas provocadas pelas tempestades de perseguição e pelos ventos da brutalidade policial.\n",
+        "\n",
+        "Vocês são veteranos do sofrimento criativo. Continuem a trabalhar com a fé de que um sofrimento injusto é redentor. Voltem para o Mississippi, voltem para o Alabama, voltem para a Carolina do Sul, voltem para a Geórgia, voltem para Luisiana, voltem para as favelas e guetos das nossas modernas cidades, sabendo que, de alguma forma, essa situação pode e será alterada. Não nos embrenhemos no vale do desespero.\n",
+        "\n",
+        "Digo-lhes hoje, meus amigos, que, apesar das dificuldades e frustrações do momento, eu ainda tenho um sonho. É um sonho profundamente enraizado no sonho americano.\n",
+        "\n",
+        "Eu tenho um sonho que um dia essa nação levantar-se-á e viverá o verdadeiro significado da sua crença: \"Consideramos essas verdades como auto-evidentes que todos os homens são criados iguais.\"\n",
+        "\n",
+        "Eu tenho um sonho que um dia, nas montanhas rubras da Geórgia, os filhos dos descendentes de escravos e os filhos dos descendentes de donos de escravos poderão sentar-se juntos à mesa da fraternidade.\n",
+        "\n",
+        "Eu tenho um sonho que um dia mesmo o estado do Mississippi, um estado desértico sufocado pelo calor da injustiça, e sufocado pelo calor da opressão, será transformado num oásis de liberdade e justiça.\n",
+        "\n",
+        "Eu tenho um sonho que meus quatro pequenos filhos um dia viverão em uma nação onde não serão julgados pela cor da pele, mas pelo conteúdo do seu caráter. Eu tenho um sonho hoje.\n",
+        "\n",
+        "Eu tenho um sonho que um dia o estado do Alabama, com seus racistas cruéis, cujo governador cospe palavras de \"interposição\" e \"anulação\", um dia bem lá no Alabama meninos negros e meninas negras possam dar-se as mãos com meninos brancos e meninas brancas, como irmãs e irmãos. Eu tenho um sonho hoje.\n",
+        "\n",
+        "Eu tenho um sonho que um dia \"todos os vales serão elevados, todas as montanhas e encostas serão niveladas; os lugares mais acidentados se tornarão planícies e os lugares tortuosos se tornarão retos e a glória do Senhor será revelada e todos os seres a verão conjuntamente\".\n",
+        "\n",
+        "Essa é a nossa esperança. Essa é a fé com a qual eu regresso ao Sul. Com essa fé nós poderemos esculpir na montanha do desespero uma pedra de esperança. Com essa fé poderemos transformar as dissonantes discórdias do nosso país em uma linda sinfonia de fraternidade.\n",
+        "\n",
+        "Com essa fé poderemos trabalhar juntos, rezar juntos, lutar juntos, ser presos juntos, defender a liberdade juntos, sabendo que um dia haveremos de ser livres. Esse será o dia, esse será o dia quando todos os filhos de Deus poderão cantar com um novo significado:\n",
+        "\n",
+        "Meu país é teu, doce terra da liberdade, de ti eu canto.\n",
+        "\n",
+        "Terra onde morreram meus pais, terra do orgulho dos peregrinos, que de cada lado das montanhas ressoe a liberdade!\n",
+        "\n",
+        "E se a América quiser ser uma grande nação, isso tem que se tornar realidade.\n",
+        "\n",
+        "E que a liberdade ressoe então do topo das montanhas mais prodigiosas de Nova Hampshire.\n",
+        "\n",
+        "Que a liberdade ressoe das poderosas montanhas de Nova Iorque.\n",
+        "\n",
+        "Que a liberdade ressoe das elevadas montanhas Allegheny da Pensilvânia.\n",
+        "\n",
+        "Que a liberdade ressoe dos cumes cobertos de neve das montanhas Rochosas do Colorado.\n",
+        "\n",
+        "Que a liberdade ressoe dos picos curvos da Califórnia.\n",
+        "\n",
+        "Mas não só isso; que a liberdade ressoe da montanha Stone da Geórgia.\n",
+        "\n",
+        "Que a liberdade ressoe da montanha Lookout do Tennessee.\n",
+        "\n",
+        "Que a liberdade ressoe de cada montanha e de cada pequena elevação do Mississippi. Que de cada encosta a liberdade ressoe.\n",
+        "\n",
+        "E quando isso acontecer, quando permitirmos que a liberdade ressoe, quando a deixarmos ressoar de cada vila e cada lugar, de cada estado e cada cidade, seremos capazes de fazer chegar mais rápido o dia em que todos os filhos de Deus, negros e brancos, judeus e gentios, protestantes e católicos, poderão dar-se as mãos e cantar as palavras da antiga canção espiritual negra:\n",
+        "\n",
+        "Finalmente livres! Finalmente livres!\n",
+        "\n",
+        "Graças a Deus Todo Poderoso, somos livres, finalmente."
+      ]
+    }
+  ]
+}
